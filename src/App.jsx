2d77517a4eca80f60167f73b4cf48dc0ce48f74a
Anwar_Hossain <ranakrphone@gmail.com>
2026-06-22 12:48:41 +0600

    Home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-gray-50 text-gray-900 font-sans antialiased">
         <Navbar />
+        <main className="grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
